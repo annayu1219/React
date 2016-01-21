09a53ee0bab305c495017927db7ce193570ab3f1
Anna Yu <ayu@thoughtworks.com>
2016-01-20 20:45:46 -0600

    Gulp build

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
 var gulp = require('gulp');
 var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
-gulp.task('browserify', function() {
-    browserify("./jsx/App.jsx")
-        .transform("babelify", {presets: ["es2015", "react"]})
+gulp.task('js', function() {
+    browserify({entries: './jsx/App.jsx', extensions: ['.jsx'], debug: true})
+        .transform('babelify', {presets: ['es2015', 'react']})
         .bundle()
-        .pipe(gulp.dest("./build"));
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('./build'));
 });
 
-gulp.task('build', ['browserify']);
+gulp.task('build', ['js']);
 gulp.task('default', ['build']);
